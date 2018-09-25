b7684d39c48e71619b366d291c03c4555316cc0b
vitorCaamargo <vitorcamargo@alunos.utfpr.edu.br>
2018-09-24 22:40:45 -0300

    Exemplos e Lógica da Máquina

--- automato_pilha.py
+++ automato_pilha.py
@@ -1,17 +1,27 @@
-# Preenche word com valores do arquivo
-def validate_word(word, config):
+def first_validations(word, config):
     valide_value = config[1]
-    valide_value = valide_value + config[3]
+
+    # Verifica se caracteres em word estão no alfabeto de entrada
     for i in word:
         if i not in valide_value:
-            print ("-3: Valor inserido na fita incorreto")
+            print ("-3: Valor inserido na palavra incorreto")
             return -3
+    
+    # Verifica se valor de epsilon não pertence ao alfabeto de entrada ou da pilha
+    if (config[3][0] in config[1] or config[3][0] in config[2]):
+        print ("-4: Epsilon inválido")
+        return -4
+
     return word
 
 def machine(config, word, transitions):
     # Cria fila para controle do fluxo
     q = []
-    validate_word(word, config)
+    stack = []
+
+    first_validations(word, config)
+
+    stack.insert(0, config[4][0]) # Insere símbolo inicial na pilha
 
     if (word == -1): # Valores inválidos na fita
         return -1
@@ -19,9 +29,8 @@ def machine(config, word, transitions):
     # Configurações da máquina
     setting = {
         "word": word,
-        "current_state": config[5][0],
-        "stack": stack,
-        "counter": 500 # Contador que 'verifica' looping
+        "current_state": config[6][0],
+        "stack": stack
     }
     
     # Insere elemento no final da fila
@@ -31,54 +40,36 @@ def machine(config, word, transitions):
         word = setting["word"]
         
         # Encontrou estado final
-        if (q[0]['current_state'] in config[6]):
-            print ("0: Computação terminada e aceita.")
-            print (setting)
-            return 0
-            
-        # Se a máquina estiver em looping
-        if (q[0]['counter'] == 0):
-            print ("-2: Computação não terminada. (interrupção por looping)")
-            print (setting)
-            return -2
-
-        # Pega posição da fita na configuração atual
-        head = q[0]['head_word']
+        if (q[0]['current_state'] in config[7]):
+            if(len(q[0]['stack']) == 0):
+                print("teste")
+            elif (q[0]['stack'][0] == config[4][0]):
+                print ("0: Computação terminada e aceita.")
+                print (setting)
+                return 0
 
         # Encontra transições possíveis
         for i in range(1, len(transitions) + 1, 1):
-            # Se estado_atual_maquina == estado_atual_fita e letra_fita == letra_transicao
-            if ((q[0]['current_state'] == transitions[i][0]) and (word[head] == transitions[i][2])):
-                # Nova disposição da fita
-                new_word = word
-                # -> Troca letra_fita por nova_letra_transicao
-                if (head > 0):
-                    new_word = (word[0:head] + transitions[i][3]) + word[head + 1:]
-                else:
-                    new_word = transitions[i][3] + word[head + 1:]
+            if ((q[0]['current_state'] == transitions[i][0]) and # Estado atual == Estado da transição
+            ((word[0] == transitions[i][1]) or (transitions[i][1] == config[3][0])) and # Letra palavra == alfabeto da transição ou alfabeto de transição == epsilon
+            ((q[0]['stack'][0] == transitions[i][2]) or (transitions[i][2] == config[3][0]))): # Topo fita == topo transição ou topo transição == epsilon
+                new_stack = {}
 
-                # Nova posição da head da fita
-                new_head_word = head
-                if (transitions[i][4] == 'R'):
-                    # Adiciona espaço em branco no final da word
-                    if (new_head_word == len(new_word) - 1):
-                        new_word = (new_word) + config[3][0]
-                    
-                    new_head_word = new_head_word + 1 # Moveu para direita
-                elif (transitions[i][4] == 'L'):
-                    # Adiciona espaço em branco no início da word
-                    if (new_head_word == 0):
-                        new_word = config[3][0] + (new_word)
-                        new_head_word += 1
-                    
-                    new_head_word = new_head_word - 1 # Moveu para esquerda
-                else:
-                    new_head_word = new_head_word # Mantêm na mesma posição
+                new_word = word
+                if (transitions[i][1] != config[3][0]):
+                    new_word = new_word[:1]
+
+                new_stack = q[0]['stack'][:]
+                if (transitions[i][2] != config[3][0]): # Alfabeto da fita != epsilon
+                    new_stack.pop(0)
+                    if (transitions[i][4] != config[3][0]):
+                        new_stack.insert(0, transitions[i][4])
+                
 
                 new_setting = {
                     "word": new_word,
-                    "current_state": transitions[i][1],
-                    "word": new_word
+                    "current_state": transitions[i][3],
+                    "stack": new_stack
                 }
 
                 q.append(new_setting) # Insere no final da fila
@@ -89,10 +80,12 @@ def machine(config, word, transitions):
             print (setting)
             return -1
         
+
+        print('\n-------')
+        print(setting)
         # Configura máquina para próximo estado
         setting["word"] = q[1]["word"]
         setting["current_state"] = q[1]["current_state"]
-        setting["head_stack"] = q[1]["head_stack"]
-        setting["counter"] = q[1]["counter"]
+        setting["stack"] = q[1]["stack"]
         q.pop(0) # Tira estado atual da fila
 
--- main.py
+++ main.py
@@ -1,34 +1,37 @@
 import sys
 import automato_pilha
 
-
 def read_file():
     config = {}
     transition = {}
+
     aux = 0
-    file_name = sys.argv[1]
-    file_name = str(file_name)
+    file_name = str(sys.argv[1])
     file = open(file_name, 'r')
+    
     for i in range(1, 8):
         conteudo = file.readline().strip('\n').split(' ')
         config[i] = conteudo
+    
     for i in file:
         aux = aux + 1
         transition[aux] = i.strip('\n').split(' ')
     return config, transition
 
-def read_stack():
-    stack = sys.argv[2]
-    return stack
+def read_word():
+    word = sys.argv[2]
+    return word
 
 def main():
     read_file()
-    read_tapes()
+    read_word()
+
     config = {}
     transitions = {}
-    stack = []
+    
     config, transitions = read_file()
-    stack = read_stack()
-    #automato_pilha.machine(config, stack, transitions)
+    word = read_word()
+    
+    automato_pilha.machine(config, word, transitions)
 
 main()
